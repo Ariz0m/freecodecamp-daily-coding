9e6e45a514ddfe92ecae6f99d34dbabc78cd1663
Ariz0m <arielviveroshour2@gmail.com>
2026-09-05 02:18:04 -0600

    pangram challenge incomplete

---
+++ 2026/september/pangram.ts
@@ -0,0 +1,25 @@
+function isPangram(sentence: string, letters: string): boolean {
+    const alphabetRegex = /[a-z]/;
+    const lettersUsedMap = new Map<string, true>(
+        sentence
+        .toLowerCase()
+        .split('')
+        .filter(char => alphabetRegex.test(char))
+        .map(char => [char, true])
+    );
+    
+    for (const char of letters) {
+        if (!lettersUsedMap.get(char)) return false;
+    }
+
+    return true;
+}
+
+console.log(isPangram("hello", "helo")) //should return true
+console.log(isPangram("hello", "hel")) // should return false
+console.log(isPangram("hello", "helow")) //should return false
+console.log(isPangram("hello world", "helowrd")) //should return true
+console.log(isPangram("Hello World!", "helowrd")) //should return true
+console.log(isPangram("Hello World!", "heliowrd")) //should return false
+console.log(isPangram("freeCodeCamp", "frcdmp")) //should return false
+console.log(isPangram("The quick brown fox jumps over the lazy dog.", "abcdefghijklmnopqrstuvwxyz"))// should return true
