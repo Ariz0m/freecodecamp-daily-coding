c80dd2073a9cd53ebac1d102ae6797786f795fa0
Ariz0m <arielviveroshour2@gmail.com>
2026-09-06 02:10:36 -0600

    Map Challenge Page

--- src/pageObjects/ChallengePage.ts
+++ src/pageObjects/ChallengePage.ts
@@ -1,1 +1,11 @@
-export {};
+import { HomePage } from "src/pageObjects/HomePage";
+
+export class ChallengePage extends HomePage {
+    get challengeTitle() {
+        return this.$('[data-playwright-test-label="challenge-title"]')
+    }
+
+    get testOutputs() {
+        return this.$$('.test-output');
+    }
+}
